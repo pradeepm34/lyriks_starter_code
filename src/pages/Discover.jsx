e9aa535857dcd1e5ca7af77363038fc29bb96800
pradeepmogantigsa <pradeep.moganti@gsa.gov>
2023-01-24 19:52:30 -0600

    search pages

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,26 +1,28 @@
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
-import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+import { useGetTopChartsQuery ,useGetSongsByGenreQuery} from '../redux/services/shazamCore';
 import { useDispatch, useSelector } from 'react-redux';
-import playerSlice from '../redux/features/playerSlice';
+import playerSlice, { selectGenreListId } from '../redux/features/playerSlice';
 
 
 const Discover = () => {
-    const { dispatch } = useDispatch();
-    const { activeSong, isPlaying } = useSelector((state) => state?.player);
-    const { data, isFetching, error } = useGetTopChartsQuery();
+    const  dispatch  = useDispatch();
+    const { activeSong, isPlaying,genreListId } = useSelector((state) => state?.player);
+    const { data, isFetching, error } = useGetSongsByGenreQuery(genreListId || "POP");
 
     if (isFetching) return <Loader title="Fetching songs" />;
 
     if (error) return <Error />;
 
+    const genreTitle = genres.find(({value})=>value === genreListId)?.title;
+
     return (
         <div className='flex flex-col'>
             <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
-                <h2 className="font-bold text-3xl text-white text-left">Discover</h2>
+                <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
                 <select
-                    onChange={() => { }}
-                    value=""
+                    onChange={(e) => { dispatch(selectGenreListId(e.target.value))}}
+                    value={genreListId || "pop"}
                     className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5">
                     {genres.map(({ title, value }) =>
                         <option key={value} value={value}>{title}</option>
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,30 +1,48 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const shazamCoreApi = createApi({
-    reducerPath: 'shazamCoreApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
-        prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', '6a684236d6msh82cf5551c865715p14bae2jsn8df3a7eb70c1');
-            return headers;
-        },
-    }),
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "6a684236d6msh82cf5551c865715p14bae2jsn8df3a7eb70c1"
+      );
+      return headers;
+    },
+  }),
 
-    endpoints: (builder) => (
-        {
-            getTopCharts: builder.query({ query: () => '/charts/world' }),
-            getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
-            getRelatedSongs: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
-            getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
-            getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` })
-        }
-    )
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `/charts/genre-world?genre_code=${genre}`,
+    }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+    }),
+    getRelatedSongs: builder.query({
+      query: ({ songid }) => `/tracks/related?track_id=${songid}`,
+    }),
+    getArtistDetails: builder.query({
+      query: (artistId) => `/artists/details?artist_id=${artistId}`,
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `/charts/country?country_code=${countryCode}`,
+    }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+    }),
+  }),
 });
 
 export const {
-    useGetTopChartsQuery,
-    useGetSongDetailsQuery,
-    useGetRelatedSongsQuery,
-    useGetArtistDetailsQuery,
-    useGetSongsByCountryQuery
+  useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetRelatedSongsQuery,
+  useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsBySearchQuery,
 } = shazamCoreApi;
